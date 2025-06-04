33daa9b44a6b2876bf890c8669f1fbd48074751b
HuongCaoHUST <caoquyhuong1662003@gmail.com>
2025-06-04 15:16:20 +0700

    update process realtime

--- src/_2_display_module/process/process_layout.py
+++ src/_2_display_module/process/process_layout.py
@@ -1,19 +1,35 @@
 import npyscreen
+import threading
+import time
 from _4_system_data import CRP_control
+
 class ProcessBox(npyscreen.BoxTitle):
     _contained_widget = npyscreen.MultiLine
     def __init__(self, screen, *args, **keywords):
+        super().__init__(screen, *args, **keywords)
+        self.name = "PROCESS LIST < PID - NAME - STATUS - CPU - MEMORY >"
+        self.editable = True
+        self.scroll_exit = True
+        self.slow_scroll = True
+        self.update_data()
+        self.start_update_thread()
+
+    def update_data(self):
         CRP_control.get_list_proc()
-        
-        # Tạo các hàng để hiển thị từ list_proc
+        sorted_procs = sorted(CRP_control.list_proc, key=lambda proc: proc['cpu_percent'], reverse=True)
         rows = []
-        for proc in CRP_control.list_proc:
+        for proc in sorted_procs:
             row = f"{proc['pid']:<6} {proc['name'][:15]:<15} {proc['status']:<10} {proc['cpu_percent']:<8} {proc['memory_percent']:<8}"
             rows.append(row)
 
-        keywords['values'] = rows
-        keywords['name'] = "PROCESS LIST < PID - NAME - STATUS >"
-        keywords['editable'] = True
-        keywords['scroll_exit'] = True
-        keywords['slow_scroll'] = True
-        super().__init__(screen, *args, **keywords)
+        self.entry_widget.values = rows
+        self.entry_widget.display()
+
+    def start_update_thread(self):
+        def update_loop():
+            while True:
+                time.sleep(0.5)
+                self.update_data()
+        thread = threading.Thread(target=update_loop)
+        thread.daemon = True  # Đảm bảo luồng dừng khi chương trình kết thúc
+        thread.start()
